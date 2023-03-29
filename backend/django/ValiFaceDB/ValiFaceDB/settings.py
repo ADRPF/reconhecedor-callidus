3534b00af0080559cf3cc2ab420b63ec9bf6728b
_NameNotFound <felipep.almeida15@gmail.com>
2023-03-28 21:47:00 -0400

    Update ValiFaceDB

--- backend/django/ValiFaceDB/ValiFaceDB/settings.py
+++ backend/django/ValiFaceDB/ValiFaceDB/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-import config
+from ValiFaceDB.SecretKey import SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "ValiFace",
 ]
 
 MIDDLEWARE = [
