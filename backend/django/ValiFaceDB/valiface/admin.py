806b1f62c745b4f1745e31d208c728c446fe2449
_NameNotFound <felipep.almeida15@gmail.com>
2023-03-30 16:41:49 -0400

    Update admin.py

--- backend/django/ValiFaceDB/valiface/admin.py
+++ backend/django/ValiFaceDB/valiface/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+import valiface.models
 
 # Register your models here.
+
+admin.site.register(valiface.models.Gerente)
+admin.site.register(valiface.models.Funcionario)
+admin.site.register(valiface.models.Ambiente)
+admin.site.register(valiface.models.Acesso)
